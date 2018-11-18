d0d9bfb97a3872e3f0a9d3b39a3bc873dc095e6f
Sebastian Shiyoko <Hyakon@linux.home>
2018-11-18 22:42:21 +0100

    a

--- Libft/ft_strsplit.c
+++ Libft/ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: pkoo <pkoo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 15:02:39 by pkoo              #+#    #+#             */
-/*   Updated: 2018/11/18 15:33:32 by pkoo             ###   ########.fr       */
+/*   Updated: 2018/11/18 22:42:01 by pkoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,33 @@ int		ccountword(const char *s, char c)
 
 char	**ft_strsplit(char const *s, char c)
 {
-	char *s1;
+	int		word;
+	char	**res;
+	int		i;
+	int len;
 
-	s1 = &(&c);
-	(void)c;
-	(void)s;
-	return (s1);
+	i = 0;
+	word = ccountword(s, c);
+	res = (char **)malloc(sizeof(char *) * word + 1);
+	if (res == NULL)
+		return (NULL);
+	res[word + 1] = 0;
+	while(*s)
+	{
+		len = ft_strlenc(s, c) + 1;
+		//do strndup 
+		res[i] = (char *)malloc(sizeof(char) * len);
+		if (res[i] == NULL)
+		{
+			while (i--)
+				free(res[i]);
+			free(res);
+			return(NULL);
+		}
+		ft_strncpy(res[i], s, len - 1);
+		res[i][len] = '\0';
+		s += len;
+		i++;
+	}
+	return (res);
 }
