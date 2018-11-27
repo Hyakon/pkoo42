9e1dbc6016b24321f0e9a247a9696d18b96f59cc
Sebastian Shiyoko <Hyakon@linux.home>
2018-11-27 22:29:52 +0100

    a

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -1,21 +1,25 @@
-#define BUFF_SIZE 1;
+#define BUFF_SIZE 1
+#include "../LibftFinal/libft.h"
+#include <fcntl.h>
+
 
 int		get_next_line(const int fd, char **line)
 {
-	const char *str;
+	static char str[1];
 	
 	while(read(fd, str, BUFF_SIZE))
 	{
-		
+		ft_putstr(str);
 	}
+	return (1);
 }
 
 int	main(int ac, char **av)
 {
 	int fd;
+	char *line;
 
 	fd = open(av[1], S_IRUSR);
-	if (fd == -1)
-		return (1);
-	
+	get_next_line(fd, &line);
+	return (1);
 }
