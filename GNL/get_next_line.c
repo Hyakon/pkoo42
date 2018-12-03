b591619d923412a4a1529315b766e6c7ec0a77bd
Paul KOO <pkoo@e1r4p11.42.fr>
2018-12-03 17:45:03 +0100

    a

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -5,81 +5,76 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pkoo <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/28 16:39:49 by pkoo              #+#    #+#             */
-     Updated: 2018/12/03 16:51:07 by pkoo             ###   ########.fr         
+/*   Created: 2018/12/03 16:32:33 by pkoo              #+#    #+#             */
+/*   Updated: 2018/12/03 17:42:40 by pkoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "get_next_line.h"
-#include <stdio.h>
 
-
-static int iscn(char *s)
+static int	looknl(char *s)
 {
 	int i;
 
 	i = 0;
-	while(*s)
+	if (s == NULL)
+		return (-1);
+	while (*s)
 	{
-
 		if (*s == '\n')
 			return (i);
-		//write(1, s,1);
 		i++;
 		s++;
 	}
- 	//ft_putchar('a');
 	return (-1);
 }
 
-int		get_next_line(const int fd, char **line)
+
+int	get_next_line(int fd, char **line)
 {
-	char buf[BUFF_SIZE + 1];
 	static char *overflow = NULL;
+	char buf[BUFF_SIZE + 1];
 	char *tmp;
 	int i;
 	int j;
 
-
 	i = 0;
- 	tmp = overflow;
-	while ((j = read(fd, buf, BUFF_SIZE)) != 0)
+	tmp = overflow;
+	*line = NULL;
+	if ((i = looknl(overflow)) >= 0)
 	{
+		*line = ft_strdupn(overflow, i);
+		tmp = overflow;
+		overflow = ft_strsub(overflow, i + 1, ft_strlen(overflow) - i);
+		free(tmp);
+		return (1);
+	}
+	while ((j = read(fd, buf, BUFF_SIZE)))
+	{
+		if (j == -1)
+			return (-1);
 		buf[j] = '\0';
-		if (j < 0)
-			return (j);
-		if (tmp == NULL)
+		if ((i = looknl(buf)) >= 0)
 		{
-				write(1, "a",1);
-			tmp = ft_strdup(buf);
-		}
- 		else
-		{
-
-			*line = tmp;
-			tmp = ft_strjoin(tmp, buf);
-
-//	ft_putendl(buf);
-			free(*line);
-		}
-		i = iscn(tmp);
-		if (i >= 0)
-		{
-			//	ft_putnbr(i);
-			ft_putstr(tmp);
-			*line = ft_strdupn(tmp, i - 1);
-			overflow = ft_strsub(tmp, i, ft_strlen(tmp));
-			free(tmp);
+			if (*line == NULL)
+				*line = ft_strdupn(buf, i);
+			else
+			{
+				tmp = ft_strsub(buf, 0, i);
+				*line = ft_strjoin(*line, tmp);
+			}
+			overflow = ft_strsub(buf, i + 1, j - i - 1);
 			return (1);
 		}
-		//ft_bzero(buf, BUFF_SIZE);
-//		i += j;
+		if (*line == NULL)
+			*line = ft_strdup(buf);
+		else
+			*line = ft_strjoin(*line, buf);
 	}
-	if (overflow)
+	if (overflow != NULL)
 	{
-		*line = ft_strdup(tmp);
-		free(tmp);
+		*line = ft_strdup(overflow);
+		free(overflow);
 		overflow = NULL;
 		return (1);
 	}
@@ -97,13 +92,12 @@ int	main(int ac, char **av)
 	{
 		(void)ac;
 		write(1, "",1);
-		//	ft_putendl(line);
+		ft_putendl(line);
 	}
+//	ft_putendl(line);
 	if (ret == -1)
-		//	printf("ERROR");
 		ft_putendl("Error");
 	else if (ret == 0)
-		//	printf("\nOK");
 		ft_putendl("\nOK");
 
 	return (1);
